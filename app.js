fdb9ca7bd54f1d28ccfe1ccc80ba3e2da4f443c9
Morning <morning@1-9.local>
2023-03-20 12:44:31 +0800

    added cloudinary

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import movieRouter from "./routes/movie-route.js";
 // import "./config/mongodb-config.js";
 // import db from "./config/mongodb-config.js"
 // import Movie from "./models/Movie.js"
+import cloudinary from "./config/cloudinary.js";
 
 const app = express();
 const port = 7000;
@@ -12,6 +13,18 @@ const port = 7000;
 app.use(bodyParser.json());
 app.use(cors());
 app.use(movieRouter);
+
+
+const res = cloudinary.v2.uploader.upload(
+    '',
+    { public_id: "olympic_flag" })
+
+res.then((data) => {
+    console.log(data);
+    console.log(data.secure_url);
+}).catch((err) => {
+    console.log(err);
+});
 
 app.listen(port, () => console.log(`listening on ${port}`));
 
